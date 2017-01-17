0c26d24ba1d4e46c962395cabff24a916549465f
ohad182 <ohad182@gmail.com>
2017-01-17 17:56:57 +0200

    Update TestPipeline.groovy

--- src/com/marvell/ci/impl/pipelines/TestPipeline.groovy
+++ src/com/marvell/ci/impl/pipelines/TestPipeline.groovy
@@ -7,56 +7,34 @@ import com.marvell.ci.model.Stage
 class TestPipeline implements Serializable {
 
     def script
-
-    def stages = []
-
-
-    // static builder(script, steps) {
-    //     steps.echo "builder - enter"
-    //     environment {
-    //         chaim = "Test-Chaim"
-    //     }
-    //     script.echo "echo from script"
-    //     steps.echo "chaim - ${env.chaim}"
-    
-    //     return new Builder(script, steps)
-    // }
-
-    // static class Builder implements Serializable {
-
-    //     def stages = []
-
-    //     def script
-
-    //     def steps
+    def stages = []   
 
         TestPipeline(def script) {
             this.script = script
-            script.echo "TestPipeline"
-            
+            script.echo "TestPipeline"            
         }  
 
         def withTestStage() {
+            Stage testStage = new Stage(script,'TestStage1')
 
-            Stage testStage = new Stage(script,'TestStage')
-
-            testStage.AddStep('Step1',new TestStep(script))
+            testStage.AddStep('Step1',new TestStep(script,'step-test1'))
             stages << testStage
         }
+   
+        def withTestStage2() {
+            Stage testStage = new Stage(script,'TestStage2')
 
-        // def build() {
-            
-        //     return new Pipeline(this)
-        // }
-
+            testStage.AddStep('Step1',new TestStep(script,'step-test2'))
+            testStage.AddStep('Step1',new TestStep(script,'step-test3'))
+            stages << testStage
+        }
+    
         def buildDefaultPipeline() {
             this.script.echo "building"
             withTestStage()
+            withTestStage2()
         }
 
- //   }
-
-  
     void execute() {
         buildDefaultPipeline()
         // `stages.each { ... }` does not work, see https://issues.jenkins-ci.org/browse/JENKINS-26481
